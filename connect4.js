4ea7f864d6a6628ca60f8932d25e8a172c9d2f39
Clay O'Dell <clay.odell.music@gmail.com>
2024-02-17 19:00:59 -0600

    final changes made to fix issues with winning and pieces not showing before the alert

--- connect4.js
+++ connect4.js
@@ -6,16 +6,16 @@
  */
 
 class Game {
-  constructor(p1, p2) {
+  constructor(p1, p2, width = 7, height = 6) {
     this.players = [p1, p2];
     this.currPlayer = p1;
-    this.WIDTH = 7;
-    this.HEIGHT = 6;
+    this.width = width;
+    this.height = height;
     this.board = [];
   }
   makeBoard() {
-    for (let y = 0; y < this.HEIGHT; y++) {
-      this.board.push(Array.from({ length: this.WIDTH }));
+    for (let y = 0; y < this.height; y++) {
+      this.board.push(Array.from({ length: this.width }));
     }
   }
   makeHtmlBoard() {
@@ -25,15 +25,15 @@ class Game {
     top.setAttribute("id", "column-top");
     // Use an arrow function to ensure 'this' refers to the Game object
     top.addEventListener("click", (evt) => this.handleClick(evt));
-    for (let x = 0; x < this.WIDTH; x++) {
+    for (let x = 0; x < this.width; x++) {
       const headCell = document.createElement("td");
       headCell.setAttribute("id", x);
       top.append(headCell);
     }
     board.append(top);
-    for (let y = 0; y < this.HEIGHT; y++) {
+    for (let y = 0; y < this.height; y++) {
       const row = document.createElement("tr");
-      for (let x = 0; x < this.WIDTH; x++) {
+      for (let x = 0; x < this.width; x++) {
         const cell = document.createElement("td");
         cell.setAttribute("id", `${y}-${x}`);
         row.append(cell);
@@ -42,7 +42,7 @@ class Game {
     }
   }
   findSpotForCol(x) {
-    for (let y = this.HEIGHT - 1; y >= 0; y--) {
+    for (let y = this.height - 1; y >= 0; y--) {
       if (!this.board[y][x]) {
         return y;
       }
@@ -63,79 +63,79 @@ class Game {
     this.gameOver = true;
   }
   handleClick(evt) {
-  if (this.gameOver) {
-    return;
-  }
-  const x = +evt.target.id;
-  const y = this.findSpotForCol(x);
-  if (y === null) {
-    return;
-  }
-  this.board[y][x] = this.currPlayer;
-  this.placeInTable(y, x);
+    if (this.gameOver) {
+      return;
+    }
+    const x = +evt.target.id;
+    const y = this.findSpotForCol(x);
+    if (y === null) {
+      return;
+    }
+    this.board[y][x] = this.currPlayer;
+    this.placeInTable(y, x);
 
-  if (this.checkForWin()) {
-    return this.endGame(`Player ${this.currPlayer.name} won!`);
+    if (this.checkForWin()) {
+      return this.endGame(`Player ${this.currPlayer.name} won!`);
+    }
+    if (this.board.every((row) => row.every((cell) => cell))) {
+      return this.endGame("Tie!");
+    }
+    this.currPlayer =
+      this.currPlayer === this.players[0] ? this.players[1] : this.players[0];
   }
-  if (this.board.every((row) => row.every((cell) => cell))) {
-    return this.endGame("Tie!");
-  }
-  this.currPlayer =
-    this.currPlayer === this.players[0] ? this.players[1] : this.players[0];
-}
-checkForWin() {
-  const _win = (cells) => {
-    return cells.every(
-      ([y, x]) =>
-        y >= 0 &&
-        y < this.HEIGHT &&
-        x >= 0 &&
-        x < this.WIDTH &&
-        this.board[y][x] === this.currPlayer
-    );
-  };
+  checkForWin() {
+    const _win = (cells) => {
+      return cells.every(
+        ([y, x]) =>
+          y >= 0 &&
+          y < this.height &&
+          x >= 0 &&
+          x < this.width &&
+          this.board[y][x] === this.currPlayer
+      );
+    };
 
-  for (let y = 0; y < this.HEIGHT; y++) {
-    for (let x = 0; x < this.WIDTH; x++) {
-      const horiz = [
-        [y, x],
-        [y, x + 1],
-        [y, x + 2],
-        [y, x + 3],
-      ];
-      const vert = [
-        [y, x],
-        [y + 1, x],
-        [y + 2, x],
-        [y + 3, x],
-      ];
-      const diagDR = [
-        [y, x],
-        [y + 1, x + 1],
-        [y + 2, x + 2],
-        [y + 3, x + 3],
-      ];
-      const diagDL = [
-        [y, x],
-        [y + 1, x - 1],
-        [y + 2, x - 2],
-        [y + 3, x - 3],
-      ];
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        const horiz = [
+          [y, x],
+          [y, x + 1],
+          [y, x + 2],
+          [y, x + 3],
+        ];
+        const vert = [
+          [y, x],
+          [y + 1, x],
+          [y + 2, x],
+          [y + 3, x],
+        ];
+        const diagDR = [
+          [y, x],
+          [y + 1, x + 1],
+          [y + 2, x + 2],
+          [y + 3, x + 3],
+        ];
+        const diagDL = [
+          [y, x],
+          [y + 1, x - 1],
+          [y + 2, x - 2],
+          [y + 3, x - 3],
+        ];
 
-      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
-        return true;
+        if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+          return true;
+        }
       }
     }
+    return false;
   }
-  return false;
-}
-startGame() {
-  this.board = [];
-  this.makeBoard();
-  this.makeHtmlBoard();
-  this.gameOver = false;
-  this.currPlayer = this.players[0];
-}
+  startGame() {
+    this.board = [];
+    this.makeBoard();
+    this.makeHtmlBoard();
+    this.gameOver = false;
+    this.currPlayer = this.players[0];
+  }
 }
 
 class Player {
@@ -145,7 +145,7 @@ class Player {
   }
 }
 
-let game = new Game();
+let game;
 document
   .querySelector("#color-form")
   .addEventListener("submit", function (evt) {
